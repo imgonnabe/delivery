24322f6b2b6f66dfb26f64e304a959cfa0e88ff5
imgonnabe <133333039+imgonnabe@users.noreply.github.com>
2023-10-23 11:59:24 +0900

    Add files via upload

---
+++ delivery/delivery/src/main/java/com/delivery/web/sample/SampleController.java
@@ -0,0 +1,13 @@
+package com.delivery.web.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+	 
+	@GetMapping("/sample")
+	public String sample() {
+		return "/sample";
+	}
+}
